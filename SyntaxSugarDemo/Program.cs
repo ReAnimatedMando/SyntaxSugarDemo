4f493f33dbbfd6fa2d8ef7a7834884ec049edeed
Aaron Tanguma <the_guma@yahoo.com>
2025-12-14 08:38:41 -0700

    added examples of explicitly and inferred typing for variables

--- SyntaxSugarDemo/Program.cs
+++ SyntaxSugarDemo/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Explicit typing. Being explicit about the variable type
+        int number = 10;
+        string work;
+        
+        // Inferred typing
+        var myVariable = 10; // the var makes it so the compiler will decide the type based on the value, in this case an integer
+        
     }
 }
